f118349608d67af36459b1dfdde03b0841a22c7a
John Brewer <brewerja@gmail.com>
2025-04-24 21:40:22 -0400

    Ruff format

--- mlbrosters.py
+++ mlbrosters.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 from urllib import request
 
-BASE_URL = 'http://mlb.com/team/roster_active.jsp'
-BULLPEN_URL = 'http://www.baseballpress.com/bullpen-usage'
+BASE_URL = "http://mlb.com/team/roster_active.jsp"
+BULLPEN_URL = "http://www.baseballpress.com/bullpen-usage"
 
 
 def get(team):
@@ -14,10 +14,10 @@ def get(team):
     contains tuples of a player's number and his name (i.e. (u'34', u'Bryce
     Harper')."""
 
-    f = request.urlopen('%s?c_id=%s' % (BASE_URL, team))
-    soup = BeautifulSoup(f.read(), 'html.parser')
+    f = request.urlopen("%s?c_id=%s" % (BASE_URL, team))
+    soup = BeautifulSoup(f.read(), "html.parser")
 
-    bodies = soup.findAll('table', {'class': 'data roster_table'})
+    bodies = soup.findAll("table", {"class": "data roster_table"})
 
     # There should be 4 tbody sets. The first is pitchers.
     pitchers = build_players(bodies[0], False)
@@ -27,19 +27,18 @@ def get(team):
 
 
 def build_players(bodies, posplayers=True):
-    rows = BeautifulSoup(str(bodies), 'html.parser').findAll('tr')
+    rows = BeautifulSoup(str(bodies), "html.parser").findAll("tr")
 
     players = []
     for row in rows[1:]:
         if row.td:
             n = row.td.string
-            if n == '—':
-                n = '66'
-            link = row.find('td', class_='dg-name_display_first_last').a
+            if n == "—":
+                n = "66"
+            link = row.find("td", class_="dg-name_display_first_last").a
             name = link.string
-            mlb_id = link['href'].split('/')[-2]
-            bats, throws = row.find('td',
-                                    class_='dg-bats_throws').string.split('/')
+            mlb_id = link["href"].split("/")[-2]
+            bats, throws = row.find("td", class_="dg-bats_throws").string.split("/")
             players.append((n, name, bats if posplayers else throws, mlb_id))
 
     return sorted(players, key=lambda p: parse_num(p[0]))
@@ -50,24 +49,24 @@ def parse_num(num):
         try:
             return int(num)
         except ValueError:
-            print('VALUE ERROR', num)
+            print("VALUE ERROR", num)
             return 66
     return 100
 
 
 def get_bullpen_ids():
     f = request.urlopen(BULLPEN_URL)
-    soup = BeautifulSoup(f.read(), 'html.parser')
-    div = soup.findAll('div', {'class': 'bullpen-usage'})
+    soup = BeautifulSoup(f.read(), "html.parser")
+    div = soup.findAll("div", {"class": "bullpen-usage"})
     bullpen_ids = set()
     names = []
     for d in div:
-        rows = d.table.findAll('tr')
+        rows = d.table.findAll("tr")
         for r in rows:
-            cols = r.findAll('td')
+            cols = r.findAll("td")
             for c in cols:
                 if c.a:
-                    bullpen_ids.add(c.a['data-mlb'])
+                    bullpen_ids.add(c.a["data-mlb"])
                     names.append(c.a.contents[0])
     return bullpen_ids
 
@@ -79,15 +78,15 @@ def separate_starters_and_bullpen(pitchers):
     return starters, bullpen
 
 
-if __name__ == '__main__':
-    for code in ['ari']:
+if __name__ == "__main__":
+    for code in ["ari"]:
         pitchers, position_players = get(code)
         starters, bullpen = separate_starters_and_bullpen(pitchers)
         for p in starters:
             print(p)
-        print('\n')
+        print("\n")
         for p in bullpen:
             print(p)
-        print('\n')
+        print("\n")
         for p in position_players:
             print(p)
--- scorecard.py
+++ scorecard.py
@@ -5,17 +5,18 @@ from pyx import text, path, canvas, unit, style, trafo, document
 
 import mlbrosters
 
-Game = namedtuple('Game', ('away_city, away_nick, away_code, '
-                           'home_city, home_nick, home_code, '
-                           'venue, date'))
+Game = namedtuple(
+    "Game",
+    ("away_city, away_nick, away_code, home_city, home_nick, home_code, venue, date"),
+)
 
 # Set default units to pts
-unit.set(defaultunit='pt')
+unit.set(defaultunit="pt")
 
 # Text Setup
 text.set(text.LatexRunner)
-text.preamble(r'\usepackage[sfdefault,condensed]{cabin}')
-text.preamble(r'\usepackage[T1]{fontenc}')
+text.preamble(r"\usepackage[sfdefault,condensed]{cabin}")
+text.preamble(r"\usepackage[T1]{fontenc}")
 
 # Line and Page Setup
 lw = unit.topt(style.linewidth.normal.width)
@@ -25,30 +26,39 @@ width = unit.topt(8.5 * unit.inch)
 height = unit.topt(11.0 * unit.inch)
 
 # Width of batter's square
-b = height / 4. / 5
+b = height / 4.0 / 5
 
 
 def get_pitcher_panel(roster, team_nickname):
     c = canvas.canvas()
 
     # Column headings
-    c.text(2.25 * b / 2, 3.5 * b + 2 * lw,
-           '%s PITCHERS' % team_nickname.upper(),
-           [text.halign.center, text.size.footnotesize])
-    columns = ['L/R', 'In', 'Out', 'IP', 'H', 'R', 'ER', 'BB', 'K', 'HR']
+    c.text(
+        2.25 * b / 2,
+        3.5 * b + 2 * lw,
+        "%s PITCHERS" % team_nickname.upper(),
+        [text.halign.center, text.size.footnotesize],
+    )
+    columns = ["L/R", "In", "Out", "IP", "H", "R", "ER", "BB", "K", "HR"]
     for i, col in enumerate(columns):
-        c.text(2.25 * b + i * 3 / 8. * b + 3 / 16. * b,
-               3.5 * b + 2 * lw,
-               col, [text.halign.center, text.size.footnotesize])
+        c.text(
+            2.25 * b + i * 3 / 8.0 * b + 3 / 16.0 * b,
+            3.5 * b + 2 * lw,
+            col,
+            [text.halign.center, text.size.footnotesize],
+        )
 
     # Roster of pitchers
-    #c.text(6 * b + 2, 7 * b / 2, roster,
-           #[text.size.scriptsize, text.parbox(width / 2.)])
+    # c.text(6 * b + 2, 7 * b / 2, roster,
+    # [text.size.scriptsize, text.parbox(width / 2.)])
 
     # Insert starting pitcher '1'
-    c.text(2.25 * b + 9 / 16. * b,
-           3.25 * b, '1',
-           [text.halign.center, text.valign.middle, text.size.large])
+    c.text(
+        2.25 * b + 9 / 16.0 * b,
+        3.25 * b,
+        "1",
+        [text.halign.center, text.valign.middle, text.size.large],
+    )
 
     # Table border
     c.stroke(path.rect(0, 0, 6 * b, 7 * b / 2))
@@ -60,12 +70,11 @@ def get_pitcher_panel(roster, team_nickname):
 
     # Table vertical lines
     for i in range(10):
-        x = 2.25 * b + i * 3 / 8. * b
+        x = 2.25 * b + i * 3 / 8.0 * b
         c.stroke(path.line(x, 0, x, 3.5 * b))
 
     # Panel border
-    c.stroke(path.rect(offset, 0,
-                       width / 2 - offset, height / 4 - offset))
+    c.stroke(path.rect(offset, 0, width / 2 - offset, height / 4 - offset))
     return c
 
 
@@ -80,38 +89,59 @@ def get_batter_panel(team_nick):
     c = canvas.canvas()
 
     # Outer border
-    c.stroke(path.rect(offset, offset,
-                       width / 2 - offset, height / 2 - offset))
+    c.stroke(path.rect(offset, offset, width / 2 - offset, height / 2 - offset))
 
     # Horizontal batter lines
     for i in range(1, 10):
-        c.stroke(path.line(0,         0.50 * height - i * b,
-                           width / 2, 0.50 * height - i * b))
+        c.stroke(path.line(0, 0.50 * height - i * b, width / 2, 0.50 * height - i * b))
 
     for i in range(1, 18, 2):
-        c.stroke(path.line(0,                 0.50 * height - i * b / 2,
-                           width / 2 - 6 * b, 0.50 * height - i * b / 2))
+        c.stroke(
+            path.line(
+                0,
+                0.50 * height - i * b / 2,
+                width / 2 - 6 * b,
+                0.50 * height - i * b / 2,
+            )
+        )
 
     # Vertical inning lines
     for i in range(1, 7):
-        c.stroke(path.line(width / 2 - i * b, 0.50 * height,
-                           width / 2 - i * b, 0.25 * height - 4 * b))
+        c.stroke(
+            path.line(
+                width / 2 - i * b,
+                0.50 * height,
+                width / 2 - i * b,
+                0.25 * height - 4 * b,
+            )
+        )
 
     # Diamonds and numbers
     diamond = get_diamond()
     for j in range(9):
         for i in range(6):
-            c.insert(diamond, [trafo.rotate(45),
-                               trafo.translate(width / 2 - (i + 0.5) * b,
-                                               height / 2 - 0.7 * b - j * b)])
-            c.text(width / 2 + (i - 6) * b + 0.60,
-                   height / 2 - j * b - 0.60,
-                   '%d' % (j + 1 + 9 * i),
-                   [text.halign.boxleft, text.valign.top, text.size.tiny])
+            c.insert(
+                diamond,
+                [
+                    trafo.rotate(45),
+                    trafo.translate(
+                        width / 2 - (i + 0.5) * b, height / 2 - 0.7 * b - j * b
+                    ),
+                ],
+            )
+            c.text(
+                width / 2 + (i - 6) * b + 0.60,
+                height / 2 - j * b - 0.60,
+                "%d" % (j + 1 + 9 * i),
+                [text.halign.boxleft, text.valign.top, text.size.tiny],
+            )
 
-    c.text(width / 2 - offset, height / 2 - 9 * b - offset,
-           team_nick, [text.halign.boxright, text.valign.top,
-                       text.size.scriptsize])
+    c.text(
+        width / 2 - offset,
+        height / 2 - 9 * b - offset,
+        team_nick,
+        [text.halign.boxright, text.valign.top, text.size.scriptsize],
+    )
 
     return c
 
@@ -119,10 +149,10 @@ def get_batter_panel(team_nick):
 def get_back_panel(game):
     c = canvas.canvas()
     y = height / 4 - 20
-    #c.text(1 * width / 6, y, get_roster(game.away_code),
-           #[text.parbox(width / 2.), text.size.scriptsize])
-    #c.text(2 * width / 6, y, get_roster(game.home_code),
-           #[text.parbox(width / 2.), text.size.scriptsize])
+    # c.text(1 * width / 6, y, get_roster(game.away_code),
+    # [text.parbox(width / 2.), text.size.scriptsize])
+    # c.text(2 * width / 6, y, get_roster(game.home_code),
+    # [text.parbox(width / 2.), text.size.scriptsize])
     return c
 
 
@@ -131,63 +161,71 @@ def get_roster(team_code, posplayers=True):
     if posplayers:
         return roster_list(pplayers)
     starters, bullpen = mlbrosters.separate_starters_and_bullpen(pitchers)
-    return roster_list(starters) + r'\\ \\' + roster_list(bullpen)
+    return roster_list(starters) + r"\\ \\" + roster_list(bullpen)
 
 
 def roster_list(players):
-    return r'\noindent' + r'\\'.join(
-        map(lambda p: '%s %s %s' % (p[0], p[1], p[2]), players))
+    return r"\noindent" + r"\\".join(
+        map(lambda p: "%s %s %s" % (p[0], p[1], p[2]), players)
+    )
 
 
 def get_front_panel(game):
     c = canvas.canvas()
 
     # Panel border
-    c.stroke(path.rect(offset, 0,
-                       width / 2 - offset, height / 4 - offset))
+    c.stroke(path.rect(offset, 0, width / 2 - offset, height / 4 - offset))
     vsw = 20
     ll = 3 * b
 
     # Home vs. Away
-    x = width / 4. - ll - vsw / 2.
+    x = width / 4.0 - ll - vsw / 2.0
     y = 4 * b
     c.stroke(path.line(x, y, x + ll, y))
-    c.text((2 * x + ll) / 2., y + 2,
-           '%s %s' % (game.away_city, game.away_nick), [text.halign.center])
+    c.text(
+        (2 * x + ll) / 2.0,
+        y + 2,
+        "%s %s" % (game.away_city, game.away_nick),
+        [text.halign.center],
+    )
 
-    c.text(width / 4., y, 'vs.', [text.halign.center])
+    c.text(width / 4.0, y, "vs.", [text.halign.center])
 
     x = x + ll + vsw
     c.stroke(path.line(x, y, x + ll, y))
-    c.text((2 * x + ll) / 2., y + 2,
-           '%s %s' % (game.home_city, game.home_nick), [text.halign.center])
+    c.text(
+        (2 * x + ll) / 2.0,
+        y + 2,
+        "%s %s" % (game.home_city, game.home_nick),
+        [text.halign.center],
+    )
 
     # At (@)
-    x = width / 4. - ll / 2.
+    x = width / 4.0 - ll / 2.0
     y = 3.5 * b
     c.stroke(path.line(x, y, x + ll, y))
-    c.text(x - 4, y, '@', [text.halign.right])
-    c.text((2 * x + ll) / 2., y + 2, game.venue, [text.halign.center])
+    c.text(x - 4, y, "@", [text.halign.right])
+    c.text((2 * x + ll) / 2.0, y + 2, game.venue, [text.halign.center])
 
     # On
     y = 3.0 * b
     c.stroke(path.line(x, y, x + ll, y))
-    c.text(x - 4, y, 'on', [text.halign.right])
-    c.text((2 * x + ll) / 2., y + 2, game.date, [text.halign.center])
+    c.text(x - 4, y, "on", [text.halign.right])
+    c.text((2 * x + ll) / 2.0, y + 2, game.date, [text.halign.center])
 
     # Start and Finish
-    x = width / 4. - b - vsw / 2.
+    x = width / 4.0 - b - vsw / 2.0
     y = 2.5 * b
     c.stroke(path.line(x, y, x + b, y))
-    c.text(x - 4, y, 'S:', [text.halign.right])
+    c.text(x - 4, y, "S:", [text.halign.right])
 
-    x = width / 4. + vsw / 2.
+    x = width / 4.0 + vsw / 2.0
     c.stroke(path.line(x, y, x + b, y))
-    c.text(x - 4, y, 'F:', [text.halign.right])
+    c.text(x - 4, y, "F:", [text.halign.right])
 
     # Linescore
     w = b / 4
-    x = width / 4. - 21.5 / 2 * w
+    x = width / 4.0 - 21.5 / 2 * w
     y = 0.75 * b
     c.insert(get_linescore(game), [trafo.translate(x, y)])
 
@@ -201,10 +239,18 @@ def get_linescore(game):
     x, y = 0, 0
     c.stroke(path.rect(x, y, 21.5 * w, b))
     c.stroke(path.line(x, y + b / 2, x + 21.5 * w, y + b / 2))
-    c.text(x + 2, y + 3 * b / 4., game.away_nick,
-           [text.halign.left, text.valign.middle, text.size.footnotesize])
-    c.text(x + 2, y + b / 4., game.home_nick,
-           [text.halign.left, text.valign.middle, text.size.footnotesize])
+    c.text(
+        x + 2,
+        y + 3 * b / 4.0,
+        game.away_nick,
+        [text.halign.left, text.valign.middle, text.size.footnotesize],
+    )
+    c.text(
+        x + 2,
+        y + b / 4.0,
+        game.home_nick,
+        [text.halign.left, text.valign.middle, text.size.footnotesize],
+    )
 
     x += 4 * w
     for i in range(13):
@@ -216,18 +262,18 @@ def get_linescore(game):
 
     # Column headers
     col_style = [text.halign.center, text.size.scriptsize]
-    x = 5 * w + w / 2.
+    x = 5 * w + w / 2.0
     y += b + 2
     for i in range(1, 13):
-        c.text(x, y, '%d' % i, col_style)
+        c.text(x, y, "%d" % i, col_style)
         x += w
 
     x += 0.25 * w
-    c.text(x, y, 'R', col_style)
+    c.text(x, y, "R", col_style)
     x += 1.5 * w
-    c.text(x, y, 'H', col_style)
+    c.text(x, y, "H", col_style)
     x += 1.5 * w
-    c.text(x, y, 'E', col_style)
+    c.text(x, y, "E", col_style)
 
     return c
 
@@ -235,18 +281,20 @@ def get_linescore(game):
 def get_scorecard(game):
     base = canvas.canvas()
 
-    #roster = get_roster(game.away_code, False)
+    # roster = get_roster(game.away_code, False)
     roster = None
     pp = get_pitcher_panel(roster, game.away_nick)
     base.insert(pp, [trafo.rotate(180), trafo.translate(width, height / 4)])
 
-    #roster = get_roster(game.home_code, False)
+    # roster = get_roster(game.home_code, False)
     pp = get_pitcher_panel(roster, game.home_nick)
     base.insert(pp, [trafo.rotate(180), trafo.translate(width, height / 2)])
 
     base.insert(get_batter_panel(game.away_nick))
-    base.insert(get_batter_panel(game.home_nick),
-                [trafo.rotate(180), trafo.translate(width, height)])
+    base.insert(
+        get_batter_panel(game.home_nick),
+        [trafo.rotate(180), trafo.translate(width, height)],
+    )
 
     base.insert(get_front_panel(game), [trafo.translate(0, height * 0.5)])
     base.insert(get_back_panel(game), [trafo.translate(0, height * 0.75)])
@@ -255,14 +303,22 @@ def get_scorecard(game):
 
 
 def write_canvas(canvas, filename):
-    page = document.page(canvas, 'scorecard',
-                         document.paperformat.Letter, centered=0, margin=0)
+    page = document.page(
+        canvas, "scorecard", document.paperformat.Letter, centered=0, margin=0
+    )
     d = document.document([page])
     d.writePSfile(filename)
 
 
-if __name__ == '__main__':
-    game = Game('Washington',  'Nationals',  'nationals',
-                'Baltimore',   'Orioles',    'orioles',
-                'Nationals Park', 'April 24, 2025')
-    write_canvas(get_scorecard(game), 'output')
+if __name__ == "__main__":
+    game = Game(
+        "Washington",
+        "Nationals",
+        "nationals",
+        "Baltimore",
+        "Orioles",
+        "orioles",
+        "Nationals Park",
+        "April 24, 2025",
+    )
+    write_canvas(get_scorecard(game), "output")
